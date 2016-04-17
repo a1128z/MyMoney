f11b2c4c5f31571dbeb47fbbfd0c56ddfc773a50
a1128z <a1128z@hotmail.com>
2016-04-17 20:18:29 +0800

    Add dbcontext by Core First  from DB

---
+++ MyMoney/Models/MoneyDb.cs
@@ -0,0 +1,21 @@
+namespace MyMoney.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class MoneyDb : DbContext
+    {
+        public MoneyDb()
+            : base("name=MoneyDb")
+        {
+        }
+
+        public virtual DbSet<AccountBook> AccountBook { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
