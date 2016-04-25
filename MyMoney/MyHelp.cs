0c261fc055b3ea0f653359aa0ef1a22215cd10d2
a1128z <a1128z@hotmail.com>
2016-04-25 23:55:28 +0800

    add html helper TxnColor to set  Income -> blue and Expenditure -> Red

---
+++ MyMoney/MyHelp.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using MyMoney.Enum;
+
+namespace MyMoney
+{
+    public static class MyHelp
+    {
+        public static string TxnColor(this HtmlHelper htmlHelper, TxnType txnType)
+        {
+            var output = string.Empty;
+            switch (txnType)
+            {
+                case TxnType.Expenditure:
+                    output = "text-danger";
+                    break;
+                case  TxnType.Income:
+                    output = "text-primary";
+                    break;
+                default:
+                    break;
+            }
+            return output;
+        }
+    }
+}
