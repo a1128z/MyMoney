7087603b2319c502bd18fb3d08775386f7a58a2f
a1128z <a1128z@hotmail.com>
2016-05-02 22:19:51 +0800

    Add Backend in Area

--- MyMoney/App_Start/RouteConfig.cs
+++ MyMoney/App_Start/RouteConfig.cs
@@ -16,8 +16,9 @@ namespace MyMoney
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional},
+                namespaces: new[] {"MyMoney.Controllers"}
+                );
         }
     }
 }
---
+++ MyMoney/Areas/Backend/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using AuthSample.Filter;
+using MyMoney.Services;
+using MyMoney.ViewModels;
+
+namespace MyMoney.Areas.Backend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAccountBookService _AccountBookService = new AccountBookService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AuthorizePlus]
+        public ActionResult Index([Bind(Include = "TxnType,Amount,Date,Remark")] MoneyTxnViewModel moneyTxnViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _AccountBookService.Add(moneyTxnViewModel);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(moneyTxnViewModel);
+        }
+
+        [ChildActionOnly]
+        public ActionResult List()
+        {
+            var moneyTxnViewModels = _AccountBookService.GetAllOrderByDate();
+            return View(moneyTxnViewModels);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AuthorizePlus]
+        public ActionResult IndexForAjax(MoneyTxnViewModel moneyTxnViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _AccountBookService.Add(moneyTxnViewModel);
+                return PartialView("List",_AccountBookService.GetAllOrderByDate());
+            }
+            return View("Index",moneyTxnViewModel);
+        }
+    };
+}
